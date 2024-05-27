6e5c24ca16d5221744d0111369344a20aaf66851
Geonwoo <55380241+inticoy@users.noreply.github.com>
2024-05-27 15:34:59 +0900

    Feat: friend backend
    * Chore: nickname + social
    
    * Feat: basic friend and request
    
    * Feat: postgres with docker compose
    
    * Feat: postgres
    
    * Chore: add .env to .gitignore
    
    * Chore: modify .env.sample
    
    * Chore: Makefile
    
    * Chore: createsuperuser automatically
    
    * Feat: friend delete
    
    * Chore: make tuple automatically
    
    * Chore: friend table renaming

---
+++ backend/django/reflected/friend/models.py
@@ -0,0 +1,50 @@
+from enum import Enum
+from django.db import models
+from django.conf import settings
+
+
+class FriendRequestStatus(Enum):
+    PENDING = 0
+    ACCEPTED = 1
+    REJECTED = 2
+
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="sent_friend_requests",
+        on_delete=models.CASCADE,
+    )
+    to_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="received_friend_requests",
+        on_delete=models.CASCADE,
+    )
+    status = models.IntegerField(
+        choices=[(tag.value, tag.name) for tag in FriendRequestStatus],
+        default=FriendRequestStatus.PENDING.value,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.from_user} -> {self.to_user} ({self.get_status_display()})"
+
+
+class Friend(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="friend_user",
+        on_delete=models.CASCADE,
+    )
+    friend = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="friend_friend",
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} & {self.friend}"
+
+    class Meta:
+        unique_together = ("user", "friend")

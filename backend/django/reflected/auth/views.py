8f77e51a9bf98fdfdc81fbb2b4947b2ba9376bdf
Geonwoo <55380241+inticoy@users.noreply.github.com>
2024-06-03 16:00:15 +0900

    Feat: Google Login (#23)
    * Chore: id and secret to env
    
    * Feat: Google Login

--- backend/django/reflected/auth/views.py
+++ backend/django/reflected/auth/views.py
@@ -12,7 +12,7 @@ from user.models import User, SocialType
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from reflected.settings import HOSTNAME, NGINX_PORT, DJANGO_PORT
+from reflected.settings import HOSTNAME, NGINX_PORT, DJANGO_PORT, FORTYTWO_CLIENT_ID, FORTYTWO_SECRET, GOOGLE_CLIENT_ID, GOOGLE_SECRET
 
 
 class OAuthFtView(APIView):
@@ -20,7 +20,7 @@ class OAuthFtView(APIView):
         base_url = "https://api.intra.42.fr/oauth/authorize"
 
         params = {
-            "client_id": "u-s4t2ud-b68c892947fb73d10628b594ee7638dc7787a3a3807166a0e6fc27fbdad1814e",
+            "client_id": FORTYTWO_CLIENT_ID,
             "redirect_uri": "http://"
             + HOSTNAME
             + ":"
@@ -43,8 +43,8 @@ class OauthFtCallbackView(APIView):
                 "https://api.intra.42.fr/oauth/token",
                 data={
                     "grant_type": "authorization_code",
-                    "client_id": "u-s4t2ud-b68c892947fb73d10628b594ee7638dc7787a3a3807166a0e6fc27fbdad1814e",
-                    "client_secret": "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",
+                    "client_id": FORTYTWO_CLIENT_ID,
+                    "client_secret": FORTYTWO_SECRET,
                     "code": code,
                     "redirect_uri": "http://"
                     + HOSTNAME
@@ -106,11 +106,93 @@ class OauthFtCallbackView(APIView):
             return Response({"error": str(e)}, status=500)
 
 
-# class OAuthGoogleView(APIView):
-#     def get(self, request, format=None):
-#         return
+class OAuthGoogleView(APIView):
+    def get(self, request, format=None):
+        base_url = "https://accounts.google.com/o/oauth2/v2/auth"
+
+        params = {
+            "client_id": GOOGLE_CLIENT_ID,
+            "redirect_uri": "http://"
+            + HOSTNAME
+            + ":"
+            + DJANGO_PORT
+            + "/v1/auth/oauth/google/callback",
+            "response_type": "code",
+            "scope": "openid email profile",
+        }
+        url = f"{base_url}?{urlencode(params)}"
+        return redirect(url)
 
 
-# class OAuthGoogleCallbackView(APIView):
-#     def get(self, request, format=None):
-#         return
+class OAuthGoogleCallbackView(APIView):
+    def get(self, request, format=None):
+        code = request.query_params.get("code")
+        if not code:
+            return Response({"error": "Code not provided"}, status=400)
+
+        try:
+            token_response = requests.post(
+                "https://oauth2.googleapis.com/token",
+                data={
+                    "grant_type": "authorization_code",
+                    "client_id": GOOGLE_CLIENT_ID,
+                    "client_secret": GOOGLE_SECRET,
+                    "code": code,
+                    "redirect_uri": "http://"
+                    + HOSTNAME
+                    + ":"
+                    + DJANGO_PORT
+                    + "/v1/auth/oauth/google/callback",
+                },
+            )
+            token_response_data = token_response.json()
+
+            if token_response.status_code != 200:
+                raise Exception(
+                    {
+                        "error": "Failed to fetch access token",
+                        "details": token_response_data,
+                        "status": token_response.status_code,
+                    }
+                )
+
+            access_token = token_response_data.get("access_token")
+
+            user_response = requests.get(
+                "https://www.googleapis.com/oauth2/v1/userinfo",
+                headers={
+                    "Authorization": f"Bearer {access_token}",
+                },
+            )
+
+            user_reponse_data = user_response.json()  # error check needed?
+            google_id = user_reponse_data.get("id")  # error check needed?
+
+            if User.objects.filter(
+                social_id=google_id, social_type=SocialType.Google.value
+            ).exists():
+                user = User.objects.get(
+                    social_type=SocialType.Google.value, social_id=google_id
+                )
+                user.last_login = timezone.now()
+                user.save()
+            else:
+                user = User.objects.create_user(
+                    email=user_reponse_data.get("email"),
+                    nickname=user_reponse_data.get("email").split("@")[0] + ".G",
+                    social_type=SocialType.Google.value,
+                    social_id=google_id,
+                )
+            refresh = RefreshToken.for_user(user)
+
+            tokens = {"access": str(refresh.access_token), "refresh": str(refresh)}
+            return redirect(
+                "http://"
+                + HOSTNAME
+                + ":"
+                + NGINX_PORT
+                + f'/#access_token={tokens["access"]}&refresh_token={tokens["refresh"]}'
+            )
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)

e5fa740b620ad03c1932b542f705152f69bc7631
Geonwoo <55380241+inticoy@users.noreply.github.com>
2024-06-12 21:48:08 +0900

    Feat: Notification Prototype
    * Tutorial: Installation
    
    * Tutorial: Implement a Chat Server
    
    * Tutorial: Rewrite as Asynchronous
    
    * Feat: Notification Prototype

--- backend/django/reflected/reflected/asgi.py
+++ backend/django/reflected/reflected/asgi.py
@@ -1,16 +1,36 @@
-"""
-ASGI config for reflected project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reflected.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reflected.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+
+from django.urls import re_path, path
+
+from chat.consumers import ChatConsumer
+from notification.consumers import NotificationConsumer
+
+from .middleware import TokenAuthMiddleware
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": TokenAuthMiddleware(
+            URLRouter(
+                [
+                    path("ws/notifications/", NotificationConsumer.as_asgi()),
+                    re_path(
+                        r"ws/chat/(?P<room_name>\w+)/$",
+                        ChatConsumer.as_asgi(),
+                    ),
+                ]
+            )
+        ),
+    }
+)

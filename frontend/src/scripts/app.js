28b69dd707ffebadedc74ca2fb64b04420b68a1a
Geonwoo <55380241+inticoy@users.noreply.github.com>
2024-07-15 22:54:46 +0900

    Feat: login and setter refactor (#49)
    * Refactor: login and setHome
    
    * login function now calls verifyToken
    
    * verifyToken, getAPI, postAPI
    
    * setHome does not do duplicate things anymore
    
    * Refactor: setNavbar
    
    * setNavbar now divided into two functions
    
    * setNavbar only set href, onclick functions
    
    * updateNavbar calls functions after access token is ready

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,17 +1,25 @@
-import { switchApp } from "/src/scripts/utils/display.js";
+import { setAppSwitch, navigateTo } from "/src/scripts/utils/display.js";
+import { setHome, updateHome } from "/src/scripts/view/home.js";
 import { login, setLogin } from "/src/scripts/view/login.js";
-import { setNavbar } from "/src/scripts/view/navbar.js";
-import { setHome } from "/src/scripts/view/home.js";
+import { setNavbar, updateNavbar } from "/src/scripts/view/navbar.js";
+import { alertNotifications } from "/src/scripts/view/navbar/notifications.js";
 
-// window.addEventListener("load", login);
-
-async function test() {
-  await login();
+function setApp() {
   setLogin();
-  switchApp();
-  setNavbar();
   setHome();
+  setNavbar();
+  setAppSwitch();
 }
-document.addEventListener("DOMContentLoaded", function () {
-  test();
+
+document.addEventListener("DOMContentLoaded", async function () {
+  setApp();
+  const loginSuccess = await login();
+  if (loginSuccess) {
+    navigateTo("app-home", false);
+    updateHome();
+    updateNavbar();
+    alertNotifications();
+  } else {
+    navigateTo("app-login", false);
+  }
 });
